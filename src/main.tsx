5bdf42f82e6e3e598af0a11ba625c3fe419510d9
CiroLee <ciromail@126.com>
2022-09-21 22:12:05 +0800

    feat: common update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import * as ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import BaseRoute from './routes';
 import 'reset-css';
+import React from 'react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
-  <RecoilRoot>
-    <BaseRoute />
-  </RecoilRoot>,
+  <React.StrictMode>
+    <RecoilRoot>
+      <BaseRoute />
+    </RecoilRoot>
+  </React.StrictMode>,
 );
