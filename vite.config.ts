72cc74201502cdadce1e2a688a319236bfa111a6
Ciro Lee <ciroli@CirodeMacBook-Pro.local>
2024-01-23 11:08:36 +0800

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import autoprefixer from 'autoprefixer';
 import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
   css: {
     postcss: {
       // 自动追加前缀
-      plugins: [require('autoprefixer')],
+      plugins: [autoprefixer()],
     },
   },
 });
